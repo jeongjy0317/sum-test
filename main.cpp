d6c789405306985859dfa2de32caf2b33676db20
Joon <jeongjy0317@gmail.com>
2021-07-07 16:54:02 +0900

    Edited main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,6 @@
 #include <stdio.h>
 #include "sum.h"
 
-int sum(int n){
-	int res = 0;
-	for (int i = 1; i <= n; ++i) res += i;
-	return res;
-}
-
 int main(){
 	int s = sum(10);
 	printf("result=%d\n", s);
